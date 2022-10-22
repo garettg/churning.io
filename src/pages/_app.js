8af82b8a51a03ca15c7ae0390c242264b11d4703
garettg <garettg@gmail.com>
2022-10-21 18:11:10 -0700

    Fix for react errors in console.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,10 @@
 // import css file
 import "../styles/index.scss";
 
+import {useEffect, useState} from "react";
 import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { GoogleAnalytics, usePageViews, event } from "nextjs-google-analytics";
+import { GoogleAnalytics, event } from "nextjs-google-analytics";
 import { ThemeProvider } from 'next-themes'
 
 import {Config} from "../../app.config";
@@ -21,34 +22,46 @@ export function reportWebVitals({ id, name, label, value }) {
 const client = new QueryClient();
 
 export default function App({ Component, pageProps }) {
-    usePageViews({ignoreHashChange: true});
 
-    return (
-        <>
-            <Head>
-                <title>{Config.appName}</title>
-                <link rel="shortcut icon" href="favicon.ico"/>
-                <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png"/>
-                <link rel="icon" type="image/svg+xml" href="icons/favicon.svg"/>
-                <link rel="alternate icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png"/>
-                <link rel="alternate icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png"/>
-                <link rel="alternate shortcut icon" href="icons/favicon.ico"/>
-                <link rel="manifest" href="icons/site.webmanifest"/>
-                <meta name="msapplication-TileColor" content="#2d89ef"/>
-                <meta name="msapplication-config" content="/public/icons/browserconfig.xml"/>
-                <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)"/>
-                <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)"/>
-            </Head>
-            <div className="app-root">
-                <GoogleAnalytics gaMeasurementId={Config.appAnalyticsId}/>
-                <QueryClientProvider client={client}>
-                    <SearchContextProvider>
-                        <ThemeProvider attribute="class">
-                            <Component {...pageProps} />
-                        </ThemeProvider>
-                    </SearchContextProvider>
-                </QueryClientProvider>
-            </div>
-        </>
-    );
+    const [showChild, setShowChild] = useState(false);
+    useEffect(() => {
+        setShowChild(true);
+    }, []);
+
+    if (!showChild) {
+        return null;
+    }
+
+    if (typeof window === 'undefined') {
+        return <></>;
+    } else {
+        return (
+            <>
+                <Head>
+                    <title>{Config.appName}</title>
+                    <link rel="shortcut icon" href="favicon.ico"/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/svg+xml" href="icons/favicon.svg"/>
+                    <link rel="alternate icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png"/>
+                    <link rel="alternate icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png"/>
+                    <link rel="alternate shortcut icon" href="icons/favicon.ico"/>
+                    <link rel="manifest" href="icons/site.webmanifest"/>
+                    <meta name="msapplication-TileColor" content="#2d89ef"/>
+                    <meta name="msapplication-config" content="/public/icons/browserconfig.xml"/>
+                    <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)"/>
+                    <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)"/>
+                </Head>
+                <div className="app-root">
+                    <GoogleAnalytics gaMeasurementId={Config.appAnalyticsId} trackPageViews={{ignoreHashChange: true}}/>
+                    <QueryClientProvider client={client}>
+                        <SearchContextProvider>
+                            <ThemeProvider attribute="class">
+                                <Component {...pageProps} />
+                            </ThemeProvider>
+                        </SearchContextProvider>
+                    </QueryClientProvider>
+                </div>
+            </>
+        );
+    }
 }
