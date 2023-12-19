e73a2910909148cf8c408e5ac9813c7cb98a9265
garettg <garettg@gmail.com>
2023-12-19 14:44:02 -0800

    Updated help text. Turn acronym search off because of search type. Update dependencies.

--- src/components/Help.js
+++ src/components/Help.js
@@ -14,15 +14,10 @@ const Help = () => {
                     <td className="border px-4 py-2 align-top whitespace-nowrap font-bold border-gray-200 dark:border-gray-700">Search</td>
                     <td className="border px-4 py-2 align-top border-gray-200 dark:border-gray-700">
                         <dl>
-                            <dt className="font-bold">Multiple terms (AND operation)</dt>
+                            <dt className="font-bold">Multiple terms</dt>
                             <dd className="pl-4">
-                                <p>To find comments that match two different words, separate using a space.</p>
-                                <p className="italic">Example: amex bonus</p>
-                            </dd>
-                            <dt className="mt-2 font-bold">Multiple terms (OR operation)</dt>
-                            <dd className="pl-4">
-                                <p>To find comments that match either of two different words, separate using "or".</p>
-                                <p className="italic">Example: barclays or citi</p>
+                                <p>Using words separated using a space by default is an "OR" operation, results could contain either.</p>
+                                <p className="italic">Example: chase sapphire</p>
                             </dd>
                             <dt className="mt-2 font-bold">Negation</dt>
                             <dd className="pl-4">
@@ -34,11 +29,6 @@ const Help = () => {
                                 <p>If you wanted to find an exact phrase, you can put the phrase in quotation marks.</p>
                                 <p className="italic">Example: "amex gold"</p>
                             </dd>
-                            <dt className="mt-2 font-bold">Combinations</dt>
-                            <dd className="pl-4">
-                                <p>You can combine the previous types of operations and group them using parentheses for advanced searching.</p>
-                                <p className="italic">Example: (csp or csr or sapphire) -freedom</p>
-                            </dd>
                         </dl>
                     </td>
                 </tr>
@@ -46,23 +36,6 @@ const Help = () => {
                     <td className="border px-4 py-2 align-top whitespace-nowrap font-bold border-gray-200 dark:border-gray-700">Author</td>
                     <td className="border px-4 py-2 align-top border-gray-200 dark:border-gray-700">
                         <p>This parameter will restrict the search to specific Reddit authors.</p>
-                        <dl>
-                            <dt className="mt-2 font-bold">Inclusive</dt>
-                            <dd className="pl-4">
-                                <p>To find comments by one author, simply set the value of author to the name. The field is not case-sensitive and allows multiple authors seperated by a comma.</p>
-                                <p className="italic">Example: username1,username2</p>
-                            </dd>
-                            <dt className="mt-2 font-bold">Exclusive</dt>
-                            <dd className="pl-4">
-                                <p>You can also use this parameter to return comments not made by specific authors.</p>
-                                <p className="italic">Example: !username</p>
-                            </dd>
-                            <dt className="mt-2 font-bold">Combination</dt>
-                            <dd className="pl-4">
-                                <p>You can combined both inclusive and exclusive by separating them with a comma.</p>
-                                <p className="italic">Example: username1,!username2</p>
-                            </dd>
-                        </dl>
                     </td>
                 </tr>
                 <tr>
