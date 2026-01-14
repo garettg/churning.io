31c48087203a0c50df4cd669bdcaf2763c0494c8
garettg <garettg@gmail.com>
2026-01-14 14:56:10 -0800

    Dependency updates and upgrades.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-// const { i18n } = require('./next-i18next.config')
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    output: 'export',
     assetPrefix: './',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
